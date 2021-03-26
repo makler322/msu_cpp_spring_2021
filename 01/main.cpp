e766b900ec0cde0a2fe5f4b86cf2d7534fbeba52
Aleksandr Vorontsov <makler322@gmail.com>
2021-03-26 13:09:46 +0300

    try 3

--- 01/main.cpp
+++ 01/main.cpp
@@ -4,32 +4,80 @@
 
 using namespace std;
 
-void test()
-{   
+void DefaultWorkTest()
+{
+    Allocator allocator;
+    allocator.makeAllocator(10);
+    assert(nullptr != allocator.alloc(5));
+	assert(nullptr != allocator.alloc(5));
+    allocator.reset();
+    assert(nullptr != allocator.alloc(2));
+	assert(nullptr != allocator.alloc(8));
+}
+
+void LimitAllocTest()
+{
+    Allocator allocator;
+    allocator.makeAllocator(5);
+    assert(nullptr != allocator.alloc(5));
+	assert(nullptr == allocator.alloc(1));
+    allocator.reset();
+    assert(nullptr == allocator.alloc(6));
+}
+
+void AllocBeforeAllocatorTest()
+{
     Allocator allocator;
     assert(nullptr == allocator.alloc(1));
-	allocator.makeAllocator(10);
-	assert(nullptr != allocator.alloc(10));
-	assert(nullptr == allocator.alloc(1));
-	allocator.reset();
-	assert(nullptr != allocator.alloc(5));
-	assert(nullptr != allocator.alloc(5));
-	assert(nullptr == allocator.alloc(1));
+    allocator.makeAllocator(5);
+    assert(nullptr != allocator.alloc(5));
+}
+
+void TwoResetTest()
+{
+    Allocator allocator;
+    allocator.makeAllocator(5);
+    assert(nullptr != allocator.alloc(5));
     allocator.reset();
-    assert(nullptr != allocator.alloc(7));
+    allocator.reset();
+    assert(nullptr != allocator.alloc(1));
+    allocator.reset();
+    allocator.reset();
+    assert(nullptr == allocator.alloc(8));
+}
+
+void FiveAllocTest()
+{
+    Allocator allocator;
+    allocator.makeAllocator(8);
+    assert(nullptr != allocator.alloc(2));
+    assert(nullptr != allocator.alloc(1));
+    assert(nullptr != allocator.alloc(1));
+    assert(nullptr != allocator.alloc(1));
+    assert(nullptr == allocator.alloc(8));
+}
+
+void AllocAfterLimitTest()
+{
+    Allocator allocator;
+    allocator.makeAllocator(8);
+    assert(nullptr != allocator.alloc(2));
     assert(nullptr == allocator.alloc(7));
-    allocator.reset();
-    allocator.reset();
-    allocator.reset();
-    assert(nullptr == allocator.alloc(11));
-    allocator.makeAllocator(1);
-    assert(nullptr == allocator.alloc(-2));
-    assert(nullptr != allocator.alloc(0));
+    assert(nullptr != allocator.alloc(1));
+    assert(nullptr == allocator.alloc(6));
+    assert(nullptr != allocator.alloc(5));
+    assert(nullptr == allocator.alloc(1));
 }
 
 int main()
 {   
-    test();
+    DefaultWorkTest();
+    LimitAllocTest();
+    AllocBeforeAllocatorTest();
+    TwoResetTest();
+    FiveAllocTest();
+    AllocAfterLimitTest();
+
     cout << "Success \n";
     return 0;
 }
