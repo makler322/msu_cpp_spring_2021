ed9353adf75f5d56706750414e44ec360fbf327c
Aleksandr Vorontsov <makler322@gmail.com>
2021-03-28 14:44:16 +0300

    try 4

--- 01/main.cpp
+++ 01/main.cpp
@@ -69,6 +69,63 @@ void AllocAfterLimitTest()
     assert(nullptr == allocator.alloc(1));
 }
 
+void ThreeAllocatorsTest()
+{
+    Allocator allocator1;
+    Allocator allocator2;
+    Allocator allocator3;
+    allocator1.makeAllocator(6);
+    allocator2.makeAllocator(7);
+    assert(nullptr == allocator3.alloc(1));
+    allocator3.makeAllocator(8);
+    assert(nullptr != allocator1.alloc(2));
+    assert(nullptr == allocator2.alloc(8));
+    assert(nullptr != allocator3.alloc(1));
+}
+
+void ResetBeforAllocatorTest()
+{
+    Allocator allocator;
+    allocator.reset();
+    allocator.makeAllocator(1);
+    assert(nullptr != allocator.alloc(1));
+    assert(nullptr == allocator.alloc(1));
+}
+
+void ResetAndAllocBeforAllocatorTest()
+{
+    Allocator allocator;
+    allocator.reset();
+    assert(nullptr == allocator.alloc(1));
+    allocator.makeAllocator(1);
+    assert(nullptr != allocator.alloc(1));
+    assert(nullptr == allocator.alloc(1));
+}
+
+void ResetAndAllocBeforThreeAllocatorsTest()
+{
+    Allocator allocator1;
+    Allocator allocator2;
+    Allocator allocator3;
+
+    allocator1.reset();
+    assert(nullptr == allocator1.alloc(1));
+    allocator1.makeAllocator(6);
+    assert(nullptr != allocator1.alloc(1));
+
+    allocator2.reset();
+    allocator2.makeAllocator(7);
+    assert(nullptr != allocator2.alloc(1));
+
+    assert(nullptr == allocator3.alloc(1));
+    allocator3.makeAllocator(8);
+    assert(nullptr != allocator3.alloc(1));
+
+    assert(nullptr != allocator1.alloc(2));
+    assert(nullptr != allocator2.alloc(5));
+    assert(nullptr != allocator3.alloc(1));
+}
+
 int main()
 {   
     DefaultWorkTest();
@@ -77,7 +134,11 @@ int main()
     TwoResetTest();
     FiveAllocTest();
     AllocAfterLimitTest();
-
+    ThreeAllocatorsTest();
+    ResetBeforAllocatorTest();
+    ResetAndAllocBeforAllocatorTest();
+    ResetAndAllocBeforThreeAllocatorsTest();
+     
     cout << "Success \n";
     return 0;
 }
