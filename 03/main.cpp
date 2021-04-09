96f9500f0273d98ea0131c62a048afddbca8b679
Ваше Имя <you@example.com>
2021-04-09 21:01:32 +0300

    try 1

--- 03/main.cpp
+++ 03/main.cpp
@@ -8,32 +8,41 @@
 
 using namespace std;
 
-void test()
+void SimpleTest()
 {
     const size_t rows = 5;
     const size_t cols = 3;
 
     Matrix m(rows, cols);
-
     assert(m.getRows() == 5);
     assert(m.getCols() == 3);
-
     m[1][2] = 5; // строка 1, колонка 2
 
     double x = m[4][1];
     assert(x == 0);
     m *= 3; // умножение на число
-
     Matrix m1(rows, cols);
 
     Matrix m2 = m1 + m;
     std::cout << m2 << std::endl;
 }
 
+void CompareTest()
+{
+    const size_t rows = 5;
+    const size_t cols = 3;
+
+    Matrix m1(rows, cols);
+    Matrix m2(rows, cols);
+    assert(m1 == m2);
+    m1[1][2] = 5;
+    assert(m1 != m2);
+}
 
 int main()
 {
-	test();
+	SimpleTest();
+    CompareTest();
 	cout << "Success!\n";
 
 	return 0;
