0c1ed10571bef51353bb96cd84564316fa63fe6c
Aleksandr <makler322@gmail.com>
2021-05-21 13:45:45 +0300

    try 2

--- 06/main.cpp
+++ 06/main.cpp
@@ -12,6 +12,80 @@ void CorrectTest()
     assert(text == "one + one = 2");
 }
 
+void EmptyTest()
+{
+    string text = format("","");
+    assert(text == "");
+}
+
+void OneArgumentTest()
+{
+    string text = format("{0}","7");
+    assert(text == "7");
+
+    string text1 = format("77{0}","7");
+    assert(text1 == "777");
+}
+
+void Many_N_Test()
+{
+    string text = format("{0}{1}{2}{3}{4}", 1, 2, 3, 4, 5);
+    assert(text == "12345");
+
+    string text1 = format("{0}{0}{0}{0}{0}", 1);
+    assert(text1 == "11111");
+
+    string text2 = format("{0}{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{7}{8}{9}{10}", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+    assert(text2 == "121314151617891011");
+
+}
+
+void WrongBracketsTest()
+{
+    
+    try 
+    {
+        std::string text = format("{}", 4);
+    }
+
+    catch(Bad_index& err) 
+    {
+        assert(err.what() == "expected '}', find not '}' at 1 element.");
+    }
+    catch(...) 
+    {
+        assert(1 == 0);
+    }
+
+    try 
+    {
+        std::string text = format("{1st}", 4);
+    }
+
+    catch(Bad_bracket& err) 
+    {
+        assert(err.what() == "expected '}', find not '}' at 2 element.");
+    }
+    catch(...) 
+    {
+        assert(1 == 0);
+    }
+
+    try 
+    {
+        std::string text = format("{0}{1}", 4);
+    }
+
+    catch(Bad_index& err) 
+    {
+        assert(err.what() == "wrong index at 5 element.");
+    }
+    catch(...) 
+    {
+        assert(1 == 0);
+    }
+
+}
 
 void DifferentParametersTest() 
 {
@@ -78,10 +152,13 @@ void IndexTest()
 int main() 
 {
     CorrectTest();
+    EmptyTest();
     DifferentParametersTest();
     BracketsTest();
     IndexTest();
-
+    OneArgumentTest();
+    Many_N_Test();
+    WrongBracketsTest();
     cout << "Success!\n";
     
     return 0;
